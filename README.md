873498f236a4d51ce7d886c6f5b57e827b4e0e1a
Chris <cyeh@ucsd.edu>
2019-08-29 13:58:55 -0700

    Pre-kernel

---
+++ README.md
@@ -0,0 +1,21 @@
+# Perceptrons Kernelized
+
+Run with: `python -u perceptron.py` in the directory with all the files included in this repository.
+
+Machine learning predictor developed with numpy and pandas.
+
+Made with a specific dataset in mind classifying a topic made in two different internet newsgroups.
+
+For more details, see pa3.pdf
+
+## Dependencies
+
+Requires python 3, as well as numpy and pandas.
+
+Easiest way to get numpy is with the command `pip3 install numpy`, and likewise for pandas `pip3 install pandas`.
+
+## TODO
+
+Make more general for other datasets.
+
+Use other kernels.
--- perceptron.py
+++ perceptron.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
+import copy
+import sys
 
 # Perceptron Algorithm to find a good weight vector w, where t is the number of passes
 def percept(p):
@@ -25,8 +27,6 @@ def percept(p):
                 w = np.add(w, y * x)
     return w
 
-# Given a kernal function,
-
 # Predict a label given the weight perceptron, where point is the row with features
 def predict(w, point):
     x = np.array(point)
@@ -51,7 +51,41 @@ def get_error(w, filename):
         total_points += 1
     return float(mistakes) / total_points
 
+# Get the 5 most significant coordinates, if for_positive is True, we get the 5 in positive class, otherwise negative
+def get_significant_coordinates(weights, for_positive):
+    w = copy.deepcopy(weights)
+    max_list = [] # Tuples of max values and its index
+    for _ in range(5):  
+        index = len(w)
+        if for_positive:
+            max = -sys.maxsize
+        else:
+            max = sys.maxsize
+        for j in range(len(w)):
+            if for_positive:     
+                if w[j] > max: 
+                    max = w[j]
+                    index = j
+            else:
+                if w[j] < max:
+                    max = w[j]
+                    index = j
+        w = np.delete(w, index) 
+        max_list.append((max, index))
+    return max_list
+
 # Find error
 if __name__ == '__main__':
-    w = percept(1)
-    print(get_error(w, 'pa3train.txt'))
+    w2 = percept(2)
+    w3 = percept(3)
+    w4 = percept(4)
+    w5 = percept(5)
+    print(get_significant_coordinates(w5, False))
+    print("2 pass on training: " + str(get_error(w2, 'pa3train.txt')))
+    print("3 pass on training: " + str(get_error(w3, 'pa3train.txt')))
+    print("4 pass on training: " + str(get_error(w4, 'pa3train.txt')))
+    print("5 pass on training: " + str(get_error(w5, 'pa3train.txt')))
+    print("2 pass on testing: " + str(get_error(w2, 'pa3test.txt')))
+    print("3 pass on testing: " + str(get_error(w2, 'pa3test.txt')))
+    print("4 pass on testing: " + str(get_error(w2, 'pa3test.txt')))
+    print("5 pass on testing: " + str(get_error(w2, 'pa3test.txt')))
